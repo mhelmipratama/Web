78b684f622b4f7a19787cb9309982635da69714c
Muhamad Helmi Pratama G <mhelmipratama@gmail.com>
2021-02-15 17:22:05 +0700

    commit keempat

--- config/proses_editprofile.php
+++ config/proses_editprofile.php
@@ -8,4 +8,4 @@ $password = $_POST["password"];
 $q = $database_connection->prepare("UPDATE `member` SET `username` = ?, 
 `password` = SHA1(?) WHERE `user`.`id` = ?");
 $q->execute([$username, $password]);
-header("Location: ../menuUtama.php");
+header("Location: ../halaman-menu-utama.php");
